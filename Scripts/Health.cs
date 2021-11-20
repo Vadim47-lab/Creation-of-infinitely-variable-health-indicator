6350caf51ad5f8d9b5759f7fb6a17ba6b0109caf
Stepanov Vadim <vadgat777@mail.ru>
2021-11-20 23:37:11 +0700

    Add files via upload

--- Scripts/Health.cs
+++ Scripts/Health.cs
@@ -8,10 +8,9 @@ public class Health : MonoBehaviour
     [SerializeField] private Button _extensionLife;
     [SerializeField] private Button _shrinkingLife;
     [SerializeField] private Text _textLife;
-    [SerializeField] private float _target;
-    [SerializeField] private float _currentStrength;
+    [SerializeField] private float _minStrength;
     [SerializeField] private float _maxStrength;
-    [SerializeField] private int _life = 100;
+    [SerializeField] private float _life = 100;
     [SerializeField] private int _difference = 10;
 
     private readonly Display _display;
@@ -29,28 +28,34 @@ public class Health : MonoBehaviour
 
     private void ExtensionLife()
     {
-        _target = Mathf.MoveTowards(_maxStrength, _currentStrength, _life += _difference);
-       
+        //_life = Mathf.MoveTowards(_maxStrength, _currentStrength, _life += _difference);
+
+        _life = Mathf.MoveTowards(_life, _maxStrength, _difference);
+
         if (_life > 100)
         {
-            _life -= _difference;
+            _life = _maxStrength;
         }
+
+        Debug.Log("_Life + = " + _life);
     }
 
     private void ShrinkingLife()
     {
-        _target = Mathf.MoveTowards(_maxStrength, _currentStrength, _life -= _difference);
+        _life = Mathf.MoveTowards(_life, _minStrength, _difference);
 
         if (_life < 0)
         {
             _life += _difference;
         }
+
+        Debug.Log("_Life - = " + _life);
     }
 }
 
 public class Display
 {
-    public void Update(Text _textLife, int _life)
+    public void Update(Text _textLife, float _life)
     {
         _textLife.text = "Количество жизни = " + _life;
     }
