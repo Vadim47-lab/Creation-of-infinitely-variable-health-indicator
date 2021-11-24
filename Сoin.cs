ce32e68e830a4e466cf59f0d7dcc3c72f1c4fa59
Stepanov Vadim <vadgat777@mail.ru>
2021-11-24 16:04:33 +0700

    Add files via upload

---
+++ Сoin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Сoin : MonoBehaviour
+{
+    [SerializeField] private GameObject _coin;
+
+    private CoinsCounter _сoinsCounter;
+
+    private void Start()
+    {
+        _сoinsCounter = gameObject.AddComponent<CoinsCounter>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            _сoinsCounter.countingСoins();
+        }
+
+        _coin.SetActive(false);
+    }
+}
