26a51c439feb91e42fb04b15dcd565b29d98974b
Stepanov Vadim <vadgat777@mail.ru>
2021-11-26 13:48:27 +0700

    Add files via upload

---
+++ Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private UnityEvent _displayHealthBar;
+    [SerializeField] private UnityEvent _increaseHealthBar;
+    [SerializeField] private UnityEvent _decreaseHealthBar;
+    [SerializeField] private Button _extensionHealth;
+    [SerializeField] private Button _shrinkingHealth;
+    [SerializeField] private float _health = 100;
+
+    private void Start()
+    {
+        _extensionHealth.onClick.AddListener(Increase);
+        _shrinkingHealth.onClick.AddListener(Decrease);
+    }
+
+    private void Increase()
+    {
+        _displayHealthBar?.Invoke();
+        _increaseHealthBar?.Invoke();
+    }
+
+    private void Decrease()
+    {
+        _displayHealthBar?.Invoke();
+        _decreaseHealthBar?.Invoke();
+    }
+}
---
+++ Scripts/HealthBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Text _textHealth;
+    [SerializeField] private float _minHealth;
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private int _difference = 10;
+
+    public void DisplayHealthBar(float _health)
+    {
+        _textHealth.text = "Количество жизни = " + _health;
+    }
+
+    public void IncreaseLife(ref float _health)
+    {
+        _slider.value = _health;
+
+        for (_health = 0; _health < _maxHealth; _health++)
+        {
+            _health = Mathf.MoveTowards(_health, _maxHealth, _difference);
+        }
+
+        if (_health > _maxHealth)
+        {
+            _health = _maxHealth;
+            _slider.value = _health;
+        }
+    }
+
+    public void DecreaseLife(ref float _health)
+    {
+        _slider.value = _health;
+
+        for (_health = 100; _health > _minHealth; _health--)
+        {
+            _health = Mathf.MoveTowards(_health, _minHealth, _difference);
+        }
+
+        if (_health < _minHealth)
+        {
+            _health += _difference;
+            _slider.value = _health;
+        }
+    }
+}
