e6f667cd7d5f01e71ef6afb26862f4c4a95f220b
Stepanov Vadim <vadgat777@mail.ru>
2021-11-29 17:39:58 +0700

    Add files via upload

--- Scripts/Health.cs
+++ Scripts/Health.cs
@@ -7,14 +7,27 @@ using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
-    [SerializeField] private UnityEvent _displayHealthBar;
-    [SerializeField] private UnityEvent _increaseHealthBar;
-    [SerializeField] private UnityEvent _decreaseHealthBar;
     [SerializeField] private Button _extensionHealth;
     [SerializeField] private Button _shrinkingHealth;
     [SerializeField] private float _health = 100;
+    [SerializeField] private float _minHealth;
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private int _difference = 10;
+
+    private SliderBar _sliderBar;
 
     private void Start()
+    {
+        _sliderBar = gameObject.AddComponent<SliderBar>();
+    }
+
+    private void OnEnable()
+    {
+        _extensionHealth.onClick.AddListener(Increase);
+        _shrinkingHealth.onClick.AddListener(Decrease);
+    }
+
+    private void OnDisable()
     {
         _extensionHealth.onClick.AddListener(Increase);
         _shrinkingHealth.onClick.AddListener(Decrease);
@@ -22,13 +35,39 @@ public class Health : MonoBehaviour
 
     private void Increase()
     {
-        _displayHealthBar?.Invoke();
-        _increaseHealthBar?.Invoke();
+        _sliderBar.DisplayHealthBar(_health);
+        IncreaseHealth();
     }
 
     private void Decrease()
     {
-        _displayHealthBar?.Invoke();
-        _decreaseHealthBar?.Invoke();
+        _sliderBar.DisplayHealthBar(_health);
+        DecreaseHealth();
+    }
+
+    public void IncreaseHealth()
+    {
+        _slider.value = _health;
+
+        _health = Mathf.MoveTowards(_health, _maxHealth, _health + _difference);
+
+        if (_health > _maxHealth)
+        {
+            _health = _maxHealth;
+            _slider.value = _health;
+        }
+    }
+
+    public void DecreaseHealth()
+    {
+        _slider.value = _health;
+
+        _health = Mathf.MoveTowards(_health, _minHealth, _health - _difference);
+
+        if (_health < _minHealth)
+        {
+            _health += _difference;
+            _slider.value = _health;
+        }
     }
 }
--- Scripts/SliderBar.cs
+++ Scripts/SliderBar.cs
@@ -7,38 +7,9 @@ public class SliderBar : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private Text _textHealth;
-    [SerializeField] private float _minHealth;
-    [SerializeField] private float _maxHealth;
-    [SerializeField] private int _difference = 10;
 
     public void DisplayHealthBar(float _health)
     {
         _textHealth.text = "Количество жизни = " + _health;
-    }
-
-    public void IncreaseHealth(float _health)
-    {
-        _slider.value = _health;
-
-        _health = Mathf.MoveTowards(_health, _maxHealth, _health + _difference);
-
-        if (_health > _maxHealth)
-        {
-            _health = _maxHealth;
-            _slider.value = _health;
-        }
-    }
-
-    public void DecreaseHealth(float _health)
-    {
-        _slider.value = _health;
-
-        _health = Mathf.MoveTowards(_health, _minHealth, _health - _difference);
-
-        if (_health < _minHealth)
-        {
-            _health += _difference;
-            _slider.value = _health;
-        }
     }
 }
